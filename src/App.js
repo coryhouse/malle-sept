58baec48cf18867c901f96c52a787036b4eac7a9
Cory House <housecor@gmail.com>
2019-09-25 07:25:55 -0500

    Add keys and state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  const courses = [
+  // useState returns an array of 2 elements:
+  // 1. variable that holds the state
+  // 2. setter
+  const [courses, setCourses] = useState([
     { id: 1, title: "Java" },
     { id: 2, title: "React" },
     { id: 3, title: "C#" }
-  ];
+  ]);
+
+  function deleteCourse(id) {
+    const newCourses = courses.filter(course => course.id !== id);
+    setCourses(newCourses); // This tells React to re-render
+  }
 
   function renderCourse(course) {
-    return <li>{course.title}</li>;
+    return (
+      <li key={course.id}>
+        <button onClick={() => deleteCourse(course.id)}>Delete</button>{" "}
+        {course.title}
+      </li>
+    );
   }
 
   return <ul>{courses.map(renderCourse)}</ul>;
