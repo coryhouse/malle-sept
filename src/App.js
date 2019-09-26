aa3a8030edb9821a70293430376f13cb10872839
Cory House <housecor@gmail.com>
2019-09-26 08:02:27 -0500

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Courses from "./Courses";
 
 function App() {
   return (
     <>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/courses">Courses</Link>
+      </nav>
       <Route path="/" component={Home} exact />
       <Route path="/courses" component={Courses} />
     </>
