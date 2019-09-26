b05c17fb6ddacda89c29765f446195330547ca38
Cory House <housecor@gmail.com>
2019-09-26 07:00:48 -0500

    AddCourse

--- src/api/courseApi.js
+++ src/api/courseApi.js
@@ -13,3 +13,16 @@ export function deleteCourse(id) {
     throw new Error("Bad network response");
   });
 }
+
+export function addCourse(course) {
+  return fetch("http://localhost:3001/courses", {
+    method: "POST", // write a new record to the database
+    body: JSON.stringify(course),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(response => {
+    if (response.ok) return response.json();
+    throw new Error("Bad network response");
+  });
+}
