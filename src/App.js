152d0131044ad23d6e83ef27746c5d7a2316516a
Cory House <housecor@gmail.com>
2019-09-26 08:08:24 -0500

    Switched to NavLink and added activeStyle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
 import Home from "./Home";
 import Courses from "./Courses";
+
+const activeStyle = {
+  color: "orange"
+};
 
 function App() {
   return (
     <>
       <nav>
-        <Link to="/">Home</Link> | <Link to="/courses">Courses</Link>
+        <NavLink activeStyle={activeStyle} to="/" exact>
+          Home
+        </NavLink>{" "}
+        |{" "}
+        <NavLink activeStyle={activeStyle} to="/courses">
+          Courses
+        </NavLink>
       </nav>
       <Route path="/" component={Home} exact />
       <Route path="/courses" component={Courses} />
